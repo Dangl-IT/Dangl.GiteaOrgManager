ca6bd6a4cb981027a18983fd12da72750b2eb6af
Georg Dangl <georg@dangl.me>
2023-06-23 14:44:19 +0200

    Add initial VS solution for tool

---
+++ src/Dangl.GiteaOrgManager/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Dangl.GiteaOrgManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
