e0f0853c96ae54c79e733c288642033e08a70713
Georg Dangl <georg@dangl.me>
2025-09-19 21:31:04 +0200

    Fix warnings

--- src/Dangl.GiteaOrgManager/OrgCreationOptions.cs
+++ src/Dangl.GiteaOrgManager/OrgCreationOptions.cs
@@ -5,19 +5,19 @@ namespace Dangl.GiteaOrgManager
     public class OrgCreationOptions
     {
         [Option('u', "url", Required = true, HelpText = "The base URL of the Gitea API instance, e.g. http://gitea.dangl.dev/api/v1")]
-        public string GiteaBaseUrl { get; set; }
+        public string GiteaBaseUrl { get; set; } = string.Empty;
 
         [Option('t', "token", Required = true, HelpText = "The Gitea access token to use for the API calls")]
-        public string GiteaAccessToken { get; set; }
+        public string GiteaAccessToken { get; set; } = string.Empty;
 
         [Option('s', "source", Required = true, HelpText = "The name of the source organization to mirror from")]
-        public string SourceOrgName { get; set; }
+        public string SourceOrgName { get; set; } = string.Empty;
 
         [Option('o', "org", Required = true, HelpText = "The name of the target organization to mirror to")]
-        public string TargetOrgName { get; set; }
+        public string TargetOrgName { get; set; } = string.Empty;
 
         [Option('e', "exclude", Required = false, HelpText = "A list of repositories to exclude from the mirroring, comma separated")]
-        public string ExcludedRepos { get; set; }
+        public string? ExcludedRepos { get; set; }
 
         [Option('m', "skip-mirror", Required = false, HelpText = "If enabled, then no automatic Gitea mirroring will be setup")]
         public bool SkipMirrorSetup { get; set; }
